4ef806d286e195360adc526461383c051d370987
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-30 19:29:40 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,7 +37,7 @@ class PetsController < ApplicationController
 
   post '/pets/:id' do
 
-    @pet = Pet.find_by(params[:id])
+    @pet = Pet.find_by_id(params[:id])
   binding.pry
     ##@pet.update(params["owner"])
     #if !params["pet"]["name"].empty?
