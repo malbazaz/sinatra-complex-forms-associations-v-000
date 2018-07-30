a54b54c558fb24fe597b90d703ba9799ed3a0742
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-30 19:39:07 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -36,7 +36,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
-
+    binding.pry
     @pet = Pet.find_by_id(params[:id])
   #binding.pry
     ##@pet.update(params["owner"])
